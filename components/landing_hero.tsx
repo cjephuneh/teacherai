c75cf4dc9164d5a6df7432ca68e2a2a406fcce0c
cjephuneh <calebjephuneh@gmail.com>
2023-11-18 21:48:13 +0300

    updates

---
+++ components/landing_hero.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import TypewriterComponent from "typewriter-effect";
+import Link from "next/link";
+
+import { Button } from "../components/ui/button";
+
+export const LandingHero = () => {
+
+  return (
+    <div className="text-white font-bold py-36 text-center space-y-5">
+      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+        <h1>The Best AI Tool for</h1>
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+          <TypewriterComponent
+            options={{
+              strings: [
+                "Cat.",
+                "Exam Generation.",
+                "Exam Review.",
+                "Assignment Generation."
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+      </div>
+      <div className="text-sm md:text-xl font-light text-zinc-400">
+        Create content using AI 10x faster.
+      </div>
+      <div className="from-purple-400 to-pink-600">
+        <Link href={"/sign-up"}>
+        {/* <Link href={isSignedIn ? "/dashboard" : "/sign-up"}> */}
+
+          <Button  className="md:text-lg p-4 md:p-6 rounded-full bg-slate-500 font-semibold">
+            Start Generating For Free
+          </Button>
+        </Link>
+      </div>
+      <div className="text-zinc-400 text-xs md:text-sm font-normal">
+        No credit card required.
+      </div>
+    </div>
+  );
+};
